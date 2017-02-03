608c3e6e745ce7cc59c14b260e84202d65c417b3
TomLandryLinet <Tom.landry.linet@gmail.com>
2017-02-02 21:48:10 -0500

    arrete euuuh151

--- src/AprGit.java
+++ src/AprGit.java
@@ -11,7 +11,7 @@
 public class AprGit {
     public static void main (String [] args){
         System.out.println("coucou c'est un test");
-        System.out.println("wesh fais pas d'la merde sur mon projet");
+        System.out.println("wesh faiur mon projet");
         System.out.println("en plus tu galere a push lolilol mdr va a la salle allersalut ");
         System.out.println("wula t'abuses");
         
